ca4553bff7a399613e4f89ec49a5478e4523997c
Yifei Gao <jasongao97@gmail.com>
2024-12-04 00:14:21 +0800

    feat: add icon for back button

--- app/chapter-two/indigobunting/page.jsx
+++ app/chapter-two/indigobunting/page.jsx
@@ -66,7 +66,21 @@ export default function ChapterTwo() {
           </div>
           <div className="font-grotesk text-right text-lg mx-10 top-1/2 transform -translate-y-1/2 flex flex-row justify-between items-center">
             <div className="pl-10">
-              <Link href="/chapter-two">return</Link>
+              <Link href="/chapter-two" className="flex items-center gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span>return</span>
+              </Link>
             </div>
             <div>
               <p className="">Species Range by Season</p>
