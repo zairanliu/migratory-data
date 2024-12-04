d91c989d26e2489b0df4a6ad097ac2770f576219
Yifei Gao <jasongao97@gmail.com>
2024-12-04 13:29:07 +0800

    feat: add "read more" button to chapter-3

--- app/chapter-three/page.jsx
+++ app/chapter-three/page.jsx
@@ -121,6 +121,22 @@ export default function ChapterThree() {
             </section>
 
             <section className="w-[100vw] h-screen relative">
+              <div className="flex flex-row items-center gap-4 absolute right-20 top-1/2 -translate-y-1/2 font-grotesk text-lg pointer-events-none">
+                {" "}
+                <p className="">Read more</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="size-4"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 8a.75.75 0 0 1 .75-.75h8.69L8.22 4.03a.75.75 0 0 1 1.06-1.06l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 0 1-1.06-1.06l3.22-3.22H2.75A.75.75 0 0 1 2 8Z"
+                    clipRule="evenodd"
+                  />
+                </svg>{" "}
+              </div>
               <FlockingSimulation />
             </section>
           </motion.div>
